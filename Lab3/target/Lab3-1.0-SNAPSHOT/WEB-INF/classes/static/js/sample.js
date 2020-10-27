946d7e6ef0e43bb06739d517c20b78e420bb1531
ivansoya <ssoya.ivan@gmail.com>
2020-10-27 22:59:23 +0300

    Laba4 update

--- Lab3/target/Lab3-1.0-SNAPSHOT/WEB-INF/classes/static/js/sample.js
+++
@@ -1,7 +0,0 @@
-let showShop = function() {
-    let div = document.createElement('div');
-    div.innerHTML = "<strong>Всем привет</strong> wtf";
-    document.body.append(div);
-    window.scrollBy(0, 100);
-};
-
